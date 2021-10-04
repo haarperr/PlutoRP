24e37580e6e9a3adc6a6e6114a5fd17687d8fc99
TheProjectSky <88754780+TheProjectSky@users.noreply.github.com>
2021-10-03 23:49:47 -0500

    c

---
+++ [Pluto-scripts]/plutorp-vehicleconfig/fxmanifest.lua
@@ -0,0 +1,173 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+  'vehiclelayouts.meta',
+  'handling.meta',
+  'vehicles.meta',
+  'carcols.meta',
+  'carvariations.meta',
+  'audioconfig/sultanrsv8_game.dat151',
+  'audioconfig/sultanrsv8_game.dat151.nametable',
+  'audioconfig/sultanrsv8_game.dat151.rel',
+  'audioconfig/sultanrsv8_sounds.dat54',
+  'audioconfig/sultanrsv8_sounds.dat54.nametable',
+  'audioconfig/sultanrsv8_sounds.dat54.rel',
+  'sfx/dlc_v8sultanrs/v8sultanrs.awc',
+  'sfx/dlc_v8sultanrs/v8sultanrs_npc.awc',
+  'audioconfig/sentinelsg4_game.dat151',
+  'audioconfig/sentinelsg4_game.dat151.nametable',
+  'audioconfig/sentinelsg4_game.dat151.rel',
+  'audioconfig/sentinelsg4_sounds.dat54',
+  'audioconfig/sentinelsg4_sounds.dat54.nametable',
+  'audioconfig/sentinelsg4_sounds.dat54.rel',
+  'sfx/dlc_sentinelsg4/sentinelsg4.awc',
+  'sfx/dlc_sentinelsg4/sentinelsg4_npc.awc',
+  'sfx/dlc_argento/argento.awc',
+  'sfx/dlc_argento/argento_npc.awc',
+  'audioconfig/argento_game.dat151.rel',
+  'audioconfig/argento_sounds.dat54.rel',
+  'audioconfig/elegyx_game.dat151',
+  'audioconfig/elegyx_game.dat151.nametable',
+  'audioconfig/elegyx_game.dat151.rel',
+  'audioconfig/elegyx_sounds.dat54',
+  'audioconfig/elegyx_sounds.dat54.nametable',
+  'audioconfig/elegyx_sounds.dat54.rel',
+  'sfx/dlc_elegyx/elegyx.awc',
+  'sfx/dlc_elegyx/elegyx_npc.awc',
+  'audioconfig/majimalm_game.dat151',
+  'audioconfig/majimalm_game.dat151.rel',
+  'audioconfig/majimalm_sounds.dat54',
+  'audioconfig/majimalm_sounds.dat54.rel',
+  'sfx/dlc_majimagt/majimagt.awc',
+  'sfx/dlc_majimagt/majimagt_npc.awc',
+  'audioconfig/monroec_amp.dat10',
+	'audioconfig/monroec_amp.dat10.nametable',
+	'audioconfig/monroec_amp.dat10.rel',
+	'audioconfig/monroec_game.dat151',
+	'audioconfig/monroec_game.dat151.nametable',
+	'audioconfig/monroec_game.dat151.rel',
+	'audioconfig/monroec_sounds.dat54',
+	'audioconfig/monroec_sounds.dat54.nametable',
+	'audioconfig/monroec_sounds.dat54.rel',
+	'sfx/dlc_monroec/monroec.awc',
+	'sfx/dlc_monroec/monroec_npc.awc',
+  'audioconfig/stratumc_amp.dat10',
+  'audioconfig/stratumc_amp.dat10.nametable',
+  'audioconfig/stratumc_amp.dat10.rel',
+  'audioconfig/stratumc_game.dat151',
+  'audioconfig/stratumc_game.dat151.nametable',
+  'audioconfig/stratumc_game.dat151.rel',
+  'audioconfig/stratumc_sounds.dat54',
+  'audioconfig/stratumc_sounds.dat54.nametable',
+  'audioconfig/stratumc_sounds.dat54.rel',
+  'sfx/dlc_zircoflow/stratumc.awc',
+  'sfx/dlc_zircoflow/stratumc_npc.awc',
+  'audioconfig/gresleyh_amp.dat10',
+  'audioconfig/gresleyh_amp.dat10.nametable',
+  'audioconfig/gresleyh_amp.dat10.rel',
+  'audioconfig/gresleyh_game.dat151',
+  'audioconfig/gresleyh_game.dat151.nametable',
+  'audioconfig/gresleyh_game.dat151.rel',
+  'audioconfig/gresleyh_sounds.dat54',
+  'audioconfig/gresleyh_sounds.dat54.nametable',
+  'audioconfig/gresleyh_sounds.dat54.rel',
+  'sfx/dlc_gresleyh/gresleyh.awc',
+  'sfx/dlc_gresleyh/gresleyh_npc.awc',
+  'audioconfig/trumpetzrc_game.dat151',
+  'audioconfig/trumpetzrc_game.dat151.nametable',
+  'audioconfig/trumpetzrc_game.dat151.rel',
+  'audioconfig/trumpetzrc_sounds.dat54',
+  'audioconfig/trumpetzrc_sounds.dat54.nametable',
+  'audioconfig/trumpetzrc_sounds.dat54.rel',
+  'audioconfig/trumpetzr_game.dat151',
+  'audioconfig/trumpetzr_game.dat151.nametable',
+  'audioconfig/trumpetzr_game.dat151.rel',
+  'audioconfig/trumpetzr_sounds.dat54',
+  'audioconfig/trumpetzr_sounds.dat54.nametable',
+  'audioconfig/trumpetzr_sounds.dat54.rel',
+  'sfx/dlc_trumpetzr/trumpetzr.awc',
+  'sfx/dlc_trumpetzr/trumpetzr_npc.awc',
+  'audioconfig/ariant_game.dat151.rel',
+  'audioconfig/ariant_sounds.dat54.rel',
+  'sfx/dlc_ariant/ariant.awc',
+  'sfx/dlc_ariant/ariant_npc.awc',
+  'audioconfig/cw2019_game.dat151',
+  'audioconfig/cw2019_game.dat151.nametable',
+  'audioconfig/cw2019_game.dat151.rel',
+  'audioconfig/cw2019_sounds.dat54',
+  'audioconfig/cw2019_sounds.dat54.nametable',
+  'audioconfig/cw2019_sounds.dat54.rel',
+  'sfx/dlc_cw2019/cw2019.awc',
+  'sfx/dlc_cw2019/cw2019_npc.awc',
+  'audioconfig/skart_game.dat151',
+  'audioconfig/skart_game.dat151.nametable',
+  'audioconfig/skart_game.dat151.rel',
+  'audioconfig/skart_sounds.dat54',
+  'audioconfig/skart_sounds.dat54.nametable',
+  'audioconfig/skart_sounds.dat54.rel',
+  'sfx/dlc_skart/skart.awc',
+  'sfx/dlc_skart/skart_npc.awc',
+  'audioconfig/roger_game.dat151',
+  'audioconfig/roger_game.dat151.nametable',
+  'audioconfig/roger_game.dat151.rel',
+  'audioconfig/roger_sounds.dat54',
+  'audioconfig/roger_sounds.dat54.nametable',
+  'audioconfig/roger_sounds.dat54.rel',
+  'sfx/dlc_roger/roger.awc',
+  'sfx/dlc_roger/roger_npc.awc',
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sentinelsg4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sentinelsg4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sentinelsg4'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/argento_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/argento_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_argento'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/elegyx_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/elegyx_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_elegyx'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/majimalm_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/majimalm_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_majimagt'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/monroec_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/monroec_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/monroec_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_monroec'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/trumpetzrc_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/trumpetzr_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/trumpetzrc_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/trumpetzr_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_trumpetzr'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ariant_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ariant_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ariant'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/cw2019_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/cw2019_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_cw2019'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sultanrsv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sultanrsv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_v8sultanrs'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/stratumc_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/stratumc_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/stratumc_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_zircoflow'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/gresleyh_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gresleyh_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gresleyh_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gresleyh'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/skart_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/skart_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_skart'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/roger_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/roger_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_roger'
+
+
+
+client_script 'vehicle_names.lua'
