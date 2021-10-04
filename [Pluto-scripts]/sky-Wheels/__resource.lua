ed497e42ce16f70beaf2fea110f1755ed193ee62
TheProjectSky <88754780+TheProjectSky@users.noreply.github.com>
2021-10-03 23:25:54 -0500

    pdmpack1

---
+++ [Pluto-scripts]/sky-Wheels/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+files {
+  'jdmrimscontentunlocks.meta',
+  'carcols.meta',
+}
+
+data_file 'JDM_RIMS_CONTENT_UNLOCKS' 'jdmrimscontentunlocks.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
